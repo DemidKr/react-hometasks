2ab8e0a249392d62179e0aa90667dc3b6144ffbd
demid <demid.krasnovidov@gmail.com>
2023-11-14 12:01:07 +0300

    Finished first homework: dice roll

--- src/pages/HomeWork1/HomeWork1.tsx
+++ src/pages/HomeWork1/HomeWork1.tsx
@@ -1,3 +1,15 @@
+import Dice from "react-dice-roll";
+import {TValue} from "react-dice-roll/dist/_types";
+import {useState} from "react";
+
 export const HomeWork1 = () => {
-  return <div>Решение домашнего задания №1</div>;
+  const [value, setValue] = useState<TValue>(6);
+
+  const handleRoll = (value: TValue) => {
+    setValue(value);
+  };
+
+  return <div className="centered-container">
+    <Dice defaultValue={value} onRoll={handleRoll} />
+  </div>
 };
