7b09d2c1fec6aa5892174d3262f6c96af9d77cb3
demid <demid.krasnovidov@gmail.com>
2023-11-14 14:38:40 +0300

    Updated first homework: dice roll

--- src/pages/HomeWork1/HomeWork1.tsx
+++ src/pages/HomeWork1/HomeWork1.tsx
@@ -1,15 +1,92 @@
-import Dice from "react-dice-roll";
-import {TValue} from "react-dice-roll/dist/_types";
-import {useState} from "react";
+import React, {useState} from "react";
+
+function randomIntFromInterval(min: number, max: number) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+export const dices = (diceNumber: number) => {
+  switch (diceNumber) {
+    case 1:
+      return (
+          <div className='center-wrapper'>
+            <div className="dice"/>
+          </div>
+      )
+    case 2:
+      return (
+          <div className='column-wrapper'>
+            <div className="dice"/>
+            <div className="dice"/>
+          </div>
+      )
+    case 3:
+      return (
+          <div className='column-wrapper'>
+            <div className="dice"/>
+            <div className="dice"/>
+            <div className="dice"/>
+          </div>
+      )
+    case 4:
+      return (
+          <div className='column-wrapper'>
+            <div className="row">
+              <div className="dice"/>
+              <div className="dice"/>
+            </div>
+            <div className="row">
+              <div className="dice"/>
+              <div className="dice"/>
+            </div>
+          </div>
+      )
+    case 5:
+      return (
+          <div className='column-wrapper'>
+            <div className="row">
+              <div className="dice"/>
+              <div className="dice"/>
+            </div>
+            <div className="dice"></div>
+            <div className="row">
+              <div className="dice"/>
+              <div className="dice"/>
+            </div>
+          </div>
+      )
+    case 6:
+      return (
+          <div className='column-wrapper'>
+            <div className="row">
+              <div className="dice"/>
+              <div className="dice"/>
+            </div>
+            <div className="row">
+              <div className="dice"/>
+              <div className="dice"/>
+            </div>
+            <div className="row">
+              <div className="dice"/>
+              <div className="dice"/>
+            </div>
+          </div>
+      )
+  }
+}
 
 export const HomeWork1 = () => {
-  const [value, setValue] = useState<TValue>(6);
+  const [value, setValue] = useState(6);
 
-  const handleRoll = (value: TValue) => {
-    setValue(value);
+  const handleRoll = () => {
+    // random number from 1 to 6
+    const newValue = randomIntFromInterval(1, 6)
+    setValue(newValue)
   };
 
   return <div className="centered-container">
-    <Dice defaultValue={value} onRoll={handleRoll} />
+      <h2>Нажми на меня!</h2>
+    <div onClick={handleRoll} className="dice-container">
+      {dices(value)}
+    </div>
   </div>
 };
