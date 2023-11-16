ac9252397023bf56373c98c5f6d1f31d931801d3
demid <demid.krasnovidov@gmail.com>
2023-11-16 16:30:07 +0300

    Finished second homework

--- src/pages/HomeWork2/HomeWork2.tsx
+++ src/pages/HomeWork2/HomeWork2.tsx
@@ -1,3 +1,30 @@
+import {RefObject, useEffect, useRef, useState} from "react";
+
 export const HomeWork2 = () => {
-  return <div>Решение домашнего задания №2</div>;
+  const [scrollPosition, setScrollPosition] = useState<number>(0)
+
+  const ref = useRef<HTMLDivElement | null>(null)
+
+  function updatePosition() {
+    const elem = document.getElementById('scroll')
+    if (elem !== null) {
+      const widgetWidth = elem.clientWidth * (elem.scrollTop / (10000 - elem.offsetHeight))
+      setScrollPosition(widgetWidth)
+    }
+  }
+
+  useEffect(() => {
+    if(ref.current === null) return;
+    ref.current?.addEventListener('scroll', updatePosition)
+  }, []);
+
+  return <div className="relative-container" id="scroll" ref={ref}>
+    <div className="scroll-widget" style={{width: scrollPosition}}/>
+    <div className="really-huge-container" >
+      <h2>Это начало</h2>
+      <h2>Это середина</h2>
+      <h2>Это конец...</h2>
+    </div>
+  </div>
+
 };
